1397ef67309a65077d75f33de6b7a32a71af9828
Thu Nghia <maymimim.1202@gmail.com>
2024-06-20 06:00:43 +0700

    240620

---
+++ shopapp-backend/shopapp/shopapp/src/main/java/com/project/shopapp/controllers/OrderCtrl.java
@@ -0,0 +1,61 @@
+package com.project.shopapp.controllers;
+
+import com.project.shopapp.dtos.OrderDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("${api.prefix}/orders")
+public class OrderCtrl {
+    @PostMapping("")
+    public ResponseEntity<?> createOrder(
+            @RequestBody @Valid OrderDTO orderDTO,
+            BindingResult result
+    ) {
+        try {
+            if (result.hasErrors()) {
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+            return ResponseEntity.ok("createOrder successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{user_id}")
+    // GET http://localhost:8088/api/v1/orders/2
+    public ResponseEntity<?> getOrders(@Valid @PathVariable("user_id") Long userId) {
+        try {
+            return ResponseEntity.ok("Lấy ra danh sách order từ user_id");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    // PUT http://localhost:8088/api/v1/orders/2
+    // công việc của admin
+    public ResponseEntity<?> updateOrder(
+            @Valid @PathVariable Long id,
+            @Valid @RequestBody OrderDTO orderDTO
+    ) {
+        return ResponseEntity.ok("Cập nhật thông tin 1 order");
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteOrder(@Valid @PathVariable Long id) {
+        //xóa mềm -> cập nhật trường active = false
+        return ResponseEntity.ok("Order deleted successfully.");
+    }
+
+}
---
+++ shopapp-backend/shopapp/shopapp/src/main/java/com/project/shopapp/controllers/ProductCtrl.java
@@ -0,0 +1,100 @@
+package com.project.shopapp.controllers;
+
+import com.project.shopapp.dtos.ProductDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("${api.prefix}/products")
+public class ProductCtrl {
+    @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> createProduct(
+            @Valid
+            @ModelAttribute ProductDTO productDTO,
+//            @RequestPart("file") MultipartFile file,
+            BindingResult result
+    ) {
+        try {
+            if (result.hasErrors()) {
+                List<String> errMessages = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+                return ResponseEntity.badRequest().body(errMessages);
+            }
+            List<MultipartFile> files = productDTO.getFiles();
+            files = files == null ? new ArrayList<MultipartFile>() : files;
+            for (MultipartFile file : files) {
+                if (file.getSize() == 0) {
+                    continue;
+                }
+                // kiểm tra kích thước file và định dạng
+                if (file.getSize() > 10 * 1024 * 1024) { //kích thước > 10MB
+                    return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body("File is too large! Maximum size is 10MB");
+                }
+                String contentType = file.getContentType();
+                if (contentType == null || !contentType.startsWith("image/")) {
+                    return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body("File must be an image");
+                }
+                // lưu file và cập nhật thumbnail trong DTO
+                String filename = storeFile(file);
+                // lưu vào đối tượng product trong DB
+                // lưu vào bảng product_images
+            }
+            return ResponseEntity.ok("Product created successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    private String storeFile(MultipartFile file) throws IOException {
+        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        // thêm UUID vào trước tên file để đảm bảo tên file là duy nhất
+        String uniqueFilename = UUID.randomUUID().toString() + "_" + filename;
+        // đường dẫn đến thư mục muốn lưu file
+        Path uploadDir = Paths.get("uploads");
+        // kiểm tra và tạo thư mục nếu nó không tồn tại
+        if (!Files.exists(uploadDir)) {
+            Files.createDirectories(uploadDir);
+        }
+        // đường dẫn đầy đủ đến file
+        Path destination = Paths.get(uploadDir.toString(), uniqueFilename);
+        // copy file vào thư mục đích
+        Files.copy(file.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
+        return uniqueFilename;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<String> getProducts(
+            @RequestParam("page") int page,
+            @RequestParam("limit") int limit
+    ) {
+        return ResponseEntity.ok("getProducts here");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getProductById(@PathVariable("id") String productId) {
+        return ResponseEntity.ok("Product with ID: " + productId);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(String.format("Product with id = %d deleted successfully", id));
+    }
+}
